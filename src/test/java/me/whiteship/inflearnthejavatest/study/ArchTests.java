11d8ce161c175cb2f610289ea7f2ddb5d8507087
Sungmin Oh <fabxoe.kor@gmail.com>
2020-05-17 18:25:30 +0900

    archUnit

---
+++ src/test/java/me/whiteship/inflearnthejavatest/study/ArchTests.java
@@ -0,0 +1,11 @@
+package me.whiteship.inflearnthejavatest.study;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import org.junit.jupiter.api.Test;
+
+public class ArchTests {
+    @Test
+    void packageDependencyTests() {
+        JavaClasses classes;
+    }
+}
